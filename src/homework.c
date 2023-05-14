9cd3c36199f7646ccc8a9c43ca62b9ddfcb4e758
CyrilAns <116091227+CyrilAns@users.noreply.github.com>
2023-05-14 15:58:24 +0200

    Neumann + normal et tangentiel

--- src/homework.c
+++ src/homework.c
@@ -1,4 +1,6 @@
 #include "fem.h"
+#include <math.h>
+
 
 // Il faut un fifrelin generaliser ce code.....
 //  (1) Ajouter l'axisymétrique !    (mandatory)
@@ -85,7 +87,89 @@ double *femElasticitySolve(femProblem *theProblem) {
                     A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] + 
                                             dphidx[i] * c * dphidx[j]) * jac * weight; }}
              for (i = 0; i < theSpace->n; i++) {
-                B[mapY[i]] -= phi[i] * g * rho * jac * weight; }}} 
+                B[mapY[i]] -= phi[i] * g * rho * jac * weight; 
+                // Calculer les dérivées normales et tangentes
+                double nx = -dphidx[i];
+                double ny = -dphidy[i];
+                double tx = dphidy[i];
+                double ty = -dphidx[i];
+
+                //Conditions en normal et tangentiel
+                int nodeIndex = map[i];
+                femBoundaryCondition** conditions = theProblem->conditions;
+                int nConditions = theProblem->nBoundaryConditions;
+
+                for (j = 0; j < nConditions; j++) {
+                    femBoundaryCondition* condition = conditions[j];
+                    if (condition->type == NEUMANN_N) {
+                        // Condition en normal
+                        double normalValue = condition->value;
+                        B[mapX[i]] += nx * normalValue * jac * weight;
+                        B[mapY[i]] += ny * normalValue * jac * weight;
+                    }
+                    else if (condition->type == NEUMANN_T) {
+                        // Condition en tangentiel
+                        double tangentValue = condition->value;
+                        B[mapX[i]] += tx * tangentValue * jac * weight;
+                        B[mapY[i]] += ty * tangentValue * jac * weight;
+                    }
+                }
+             }
+
+             //Conditions de Neumann
+             int nEdges = theGeometry->theEdges->nElem;
+             int nLocalEdge = theGeometry->theEdges->nLocalNode;
+             int nBoundaryConditions = theProblem->nBoundaryConditions;
+             femBoundaryCondition** conditions = theProblem->conditions;
+             femNodes* edgesNodes = theGeometry->theEdges->nodes;
+
+             for (iEdge = 0; iEdge < nEdges; iEdge++) {
+                 for (i = 0; i < nLocalEdge; i++) {
+                     map[i] = theGeometry->theEdges->elem[iEdge * nLocalEdge + i];
+                     mapX[i] = 2 * map[i];
+                     mapY[i] = 2 * map[i] + 1;
+                     x[i] = edgesNodes->X[map[i]];
+                     y[i] = edgesNodes->Y[map[i]];
+                 }
+
+                 double edgeLength = sqrt(pow(x[1] - x[0], 2) + pow(y[1] - y[0], 2));
+                 double* edgeNormal = malloc(2 * sizeof(double));
+                 edgeNormal[0] = (y[1] - y[0]) / edgeLength;
+                 edgeNormal[1] = -(x[1] - x[0]) / edgeLength;
+
+                 for (iInteg = 0; iInteg < theRule->n; iInteg++) {
+                     double xsi = theRule->xsi[iInteg];
+                     double eta = theRule->eta[iInteg];
+                     double weight = theRule->weight[iInteg];
+                     femDiscretePhi2(theSpace, xsi, eta, phi);
+                     femDiscreteDphi2(theSpace, xsi, eta, dphidxsi, dphideta);
+
+                     double jac = edgeLength / 2.0;
+                     double normalJac = jac * weight;
+
+                     for (i = 0; i < theSpace->n; i++) {
+                         double neumannValue = 0.0;
+                         for (j = 0; j < nBoundaryConditions; j++) {
+                             femBoundaryCondition* condition = conditions[j];
+                             if (condition->type == NEUMANN_N) {
+                                 neumannValue += condition->value * phi[i];
+                             }
+                             else if (condition->type == NEUMANN_T) {
+                                 double tangentComponent = condition->value * edgeNormal[0];
+                                 neumannValue += tangentComponent * dphidxsi[i] +
+                                     tangentComponent * dphideta[i];
+                             }
+                         }
+                         B[mapX[i]] += neumannValue * normalJac * edgeNormal[0];
+                         B[mapY[i]] += neumannValue * normalJac * edgeNormal[1];
+                     }
+                 }
+
+                 free(edgeNormal);
+             }
+
+        }
+    } 
   
     int *theConstrainedNodes = theProblem->constrainedNodes;     
     for (int i=0; i < theSystem->size; i++) {
@@ -95,3 +179,18 @@ double *femElasticitySolve(femProblem *theProblem) {
                             
     return femFullSystemEliminate(theSystem);
 }
+
+
+
+//BROUILLION:
+// Calcul du vecteur des contraintes Neumann
+//double neumannX = 0.0;
+///double neumannY = 0.0;
+//for (i = 0; i < theSpace->n; i++) {
+    //neumannX += phi[i];
+    //neumannY += phi[i];// *theProblem->conditions[map[i]]->value* dphidy[i]}
+
+// Ajout des contributions des conditions de Neumann dans le système
+//for (i = 0; i < theSpace->n; i++) {
+    //B[mapX[i]] += neumannX * jac * weight;
+    //B[mapY[i]] += neumannY * jac * weight;}
