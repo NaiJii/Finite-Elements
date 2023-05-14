da237d3840e80af3bc7c8a6eaddcafe218185f65
jii <37872805+NaiJii@users.noreply.github.com>
2023-05-14 13:24:31 +0200

    fix

--- ProjectPreProcessor/src/main.c
+++ ProjectPreProcessor/src/main.c
@@ -9,119 +9,115 @@
  *  All rights reserved.
  *
  */
- 
+
 #include "glfem.h"
 
 int main(void)
-{  
+{
+	//
+	//  -1- Construction de la geometrie
+	//
 
-//
-//  -1- Construction de la geometrie 
-//
+	double Lx = 1.0;
+	double Ly = 1.0;
+	geoInitialize();
+	femGeo* theGeometry = geoGetGeometry();
 
-    double Lx = 1.0;
-    double Ly = 1.0;     
-    geoInitialize();
-    femGeo* theGeometry = geoGetGeometry();
-    
-    theGeometry->LxPlate     =  Lx;
-    theGeometry->LyPlate     =  Ly;     
-    theGeometry->h           =  Lx * 0.05;    
-    theGeometry->elementType = FEM_QUAD;
-  
-//    geoMeshGenerate();      // Utilisation de OpenCascade
-    
-//  geoMeshGenerateGeo();   // Utilisation de outils de GMSH  
-                            // Attention : les entités sont différentes !
-                            // On a aussi inversé la géomtrie pour rire !
-                            
-    geoMeshGenerateGeoFile("../data/mesh.geo");   // Lecture fichier geo
-  
-    geoMeshImport();
-    geoSetDomainName(0,"Symetry");
-    geoSetDomainName(7,"Bottom");
-    geoMeshWrite("../data/mesh.txt");
-          
-//
-//  -2- Definition du probleme
-//
-    
-    double E   = 211.e9;
-    double nu  = 0.3;
-    double rho = 7.85e3; 
-    double g   = 9.81;
-    femProblem* theProblem = femElasticityCreate(theGeometry,E,nu,rho,g,PLANAR_STRAIN);
-    femElasticityAddBoundaryCondition(theProblem,"Symetry",DIRICHLET_X,0.0);
-    femElasticityAddBoundaryCondition(theProblem,"Bottom",DIRICHLET_Y,0.0);
-    femElasticityPrint(theProblem);
-    femElasticityWrite(theProblem,"../data/problem.txt");
- 
+	theGeometry->LxPlate = Lx;
+	theGeometry->LyPlate = Ly;
+	theGeometry->h = Lx * 0.05;
+	theGeometry->elementType = FEM_QUAD;
 
-//
-//  -3- Champ de la taille de référence du maillage
-//
+	//    geoMeshGenerate();      // Utilisation de OpenCascade
 
-    double *meshSizeField = malloc(theGeometry->theNodes->nNodes*sizeof(double));
-    femNodes *theNodes = theGeometry->theNodes;
-    for(int i=0; i < theNodes->nNodes; ++i)
-        meshSizeField[i] = theGeometry->geoSize(theNodes->X[i], theNodes->Y[i]);
-    double hMin = femMin(meshSizeField,theNodes->nNodes);  
-    double hMax = femMax(meshSizeField,theNodes->nNodes);  
-    printf(" ==== Global requested h : %14.7e \n",theGeometry->h);
-    printf(" ==== Minimum h          : %14.7e \n",hMin);
-    printf(" ==== Maximum h          : %14.7e \n",hMax);
-    
-//
-//  -4- Visualisation 
-//  
-    
-    int mode = 1; 
-    int domain = 0;
-    int freezingButton = FALSE;
-    double t, told = 0;
-    char theMessage[MAXNAME];
-   
- 
-    GLFWwindow* window = glfemInit("EPL1110 : Project 2022-23 ");
-    glfwMakeContextCurrent(window);
+	//  geoMeshGenerateGeo();   // Utilisation de outils de GMSH
+								// Attention : les entités sont différentes !
+								// On a aussi inversé la géomtrie pour rire !
 
-    do {
-        int w,h;
-        glfwGetFramebufferSize(window,&w,&h);
-        glfemReshapeWindows(theGeometry->theNodes,w,h);
+	geoMeshGenerateGeoFile("../../../data/mesh.geo");   // Lecture fichier geo
 
-        t = glfwGetTime();  
-        if (glfwGetKey(window,'D') == GLFW_PRESS) { mode = 0;}
-        if (glfwGetKey(window,'V') == GLFW_PRESS) { mode = 1;}
-        if (glfwGetKey(window,'N') == GLFW_PRESS && freezingButton == FALSE) { domain++; freezingButton = TRUE; told = t;}
-        
-        if (t-told > 0.5) {freezingButton = FALSE; }
-        if (mode == 1) {
-            glfemPlotField(theGeometry->theElements,meshSizeField);
-            glfemPlotMesh(theGeometry->theElements); 
-            sprintf(theMessage, "Number of elements : %d ",theGeometry->theElements->nElem);
-            glColor3f(1.0,0.0,0.0); glfemMessage(theMessage); }
-        if (mode == 0) {
-            domain = domain % theGeometry->nDomains;
-            glfemPlotDomain( theGeometry->theDomains[domain]); 
-            sprintf(theMessage, "%s : %d ",theGeometry->theDomains[domain]->name,domain);
-             glColor3f(1.0,0.0,0.0); glfemMessage(theMessage);  }
-            
-         glfwSwapBuffers(window);
-         glfwPollEvents();
-    } while( glfwGetKey(window,GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-             glfwWindowShouldClose(window) != 1 );
-            
-    // Check if the ESC key was pressed or the window was closed
+	geoMeshImport();
+	geoSetDomainName(0, "Symetry");
+	geoSetDomainName(7, "Bottom");
+	geoMeshWrite("../../../data/mesh.txt");
 
-    free(meshSizeField);
-    femElasticityFree(theProblem) ; 
-    geoFree();
-    glfwTerminate(); 
-    
-    exit(EXIT_SUCCESS);
-    return 0;  
+	//
+	//  -2- Definition du probleme
+	//
+
+	double E = 211.e9;
+	double nu = 0.3;
+	double rho = 7.85e3;
+	double g = 9.81;
+	femProblem* theProblem = femElasticityCreate(theGeometry, E, nu, rho, g, PLANAR_STRAIN);
+	femElasticityAddBoundaryCondition(theProblem, "Symetry", DIRICHLET_X, 0.0);
+	femElasticityAddBoundaryCondition(theProblem, "Bottom", DIRICHLET_Y, 0.0);
+	femElasticityPrint(theProblem);
+	femElasticityWrite(theProblem, "../../../data/problem.txt");
+
+	//
+	//  -3- Champ de la taille de référence du maillage
+	//
+
+	double* meshSizeField = malloc(theGeometry->theNodes->nNodes * sizeof(double));
+	femNodes* theNodes = theGeometry->theNodes;
+	for (int i = 0; i < theNodes->nNodes; ++i)
+		meshSizeField[i] = theGeometry->geoSize(theNodes->X[i], theNodes->Y[i]);
+	double hMin = femMin(meshSizeField, theNodes->nNodes);
+	double hMax = femMax(meshSizeField, theNodes->nNodes);
+	printf(" ==== Global requested h : %14.7e \n", theGeometry->h);
+	printf(" ==== Minimum h          : %14.7e \n", hMin);
+	printf(" ==== Maximum h          : %14.7e \n", hMax);
+
+	//
+	//  -4- Visualisation
+	//
+
+	int mode = 1;
+	int domain = 0;
+	int freezingButton = FALSE;
+	double t, told = 0;
+	char theMessage[MAXNAME];
+
+	GLFWwindow* window = glfemInit("EPL1110 : Project 2022-23 ");
+	glfwMakeContextCurrent(window);
+
+	do {
+		int w, h;
+		glfwGetFramebufferSize(window, &w, &h);
+		glfemReshapeWindows(theGeometry->theNodes, w, h);
+
+		t = glfwGetTime();
+		if (glfwGetKey(window, 'D') == GLFW_PRESS) { mode = 0; }
+		if (glfwGetKey(window, 'V') == GLFW_PRESS) { mode = 1; }
+		if (glfwGetKey(window, 'N') == GLFW_PRESS && freezingButton == FALSE) { domain++; freezingButton = TRUE; told = t; }
+
+		if (t - told > 0.5) { freezingButton = FALSE; }
+		if (mode == 1) {
+			glfemPlotField(theGeometry->theElements, meshSizeField);
+			glfemPlotMesh(theGeometry->theElements);
+			sprintf(theMessage, "Number of elements : %d ", theGeometry->theElements->nElem);
+			glColor3f(1.0, 0.0, 0.0); glfemMessage(theMessage);
+		}
+		if (mode == 0) {
+			domain = domain % theGeometry->nDomains;
+			glfemPlotDomain(theGeometry->theDomains[domain]);
+			sprintf(theMessage, "%s : %d ", theGeometry->theDomains[domain]->name, domain);
+			glColor3f(1.0, 0.0, 0.0); glfemMessage(theMessage);
+		}
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) != 1);
+
+	// Check if the ESC key was pressed or the window was closed
+
+	free(meshSizeField);
+	femElasticityFree(theProblem);
+	geoFree();
+	glfwTerminate();
+
+	exit(EXIT_SUCCESS);
+	return 0;
 }
-
-
- 
